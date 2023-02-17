b8ef9952c110f5008037ab7e7de0e6d8528fe40f
Ray-mocca <raymond.sandi@outlook.com>
2023-02-16 21:12:01 -0600

    Agregando archivo de clase 2

---
+++ clase2.py
@@ -0,0 +1,20 @@
+# While True:
+#     print("ejemplo de prueba")
+
+
+
+# keep_going = True
+# while keep_going == True:
+#     print("and again")
+#     keep_going = False
+#     print("One more time")
+
+
+
+#Controlar un bucle
+
+counter = 5
+
+while counter <= 10:
+    print(counter)
+    counter += 1
